b59aeb22e9f0589753199f9403a56d0289ab535c
Xavier VALETTE <xaviervalette@outlook.com>
2022-03-03 10:33:24 +0100

    fix BIGGAME event and enhance frontend

--- app/getMapImg.py
+++ app/getMapImg.py
@@ -13,7 +13,6 @@ frontPath = os.environ["BRAWL_COACH_FRONTPATH"]
 backPath = os.environ["BRAWL_COACH_BACKPATH"]
 logPath = os.environ["BRAWL_COACH_LOGPATH"]
 token = os.environ["BRAWL_COACH_TOKEN"]
-baseUrl = os.environ["BRAWL_STARS_API_BASE_URL"]
 currentBattlePath = dataPath+"/battles/current"
 allBattlePath = dataPath+"/battles/all"
 webImgPath = frontPath+"/app/static/img"
@@ -44,39 +43,38 @@ for site in sites:
     urls = [img['src'] for img in img_tags]
 
     for url in urls:
-        filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
-        if not filename:
-            print("Regex didn't match with the url: {}".format(url))
-            continue
-        print(url)
-        pictureName=filename.group(1)
-        pictureName=pictureName.replace("-"," ").upper()
-        pictureName=pictureName.replace(" 2","")
-        pictureName=pictureName.replace("'","")
-        pictureName=pictureName.replace(".PNG",".JPG")
-        pictureName=pictureName.upper()
-        pictureName=pictureName.replace(" ","")
-        if(modes[i]=="DUOSHOWDOWN"):
-            pictureName=pictureName.replace("DUO","")
+        if ".png" in url:
+            filename=url.split("/")[-1]
+            print(filename)
+            print(url)
+            pictureName=filename.lower()
+            pictureName=pictureName.replace("-"," ")
+            pictureName=pictureName.replace(" 2","")
+            pictureName=pictureName.replace("'","")
+            pictureName=pictureName.replace(".png","")
+            pictureName=pictureName.replace(".","")
+            pictureName=pictureName+".png"
+            pictureName=pictureName.replace(" ","")
+            if(modes[i]=="DUOSHOWDOWN"):
+                pictureName=pictureName.replace("duo","")
+                
+            print(pictureName)
 
+            filename=webImgPath+"/maps/"+modes[i].lower()+"/"+pictureName
+            print(filename)
+            if not os.path.exists(os.path.dirname(filename)):
+                try:
+                    os.makedirs(os.path.dirname(filename))
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
 
-        print(pictureName)
-
-        filename=webImgPath+"/maps/"+modes[i]+"/"+pictureName
-        print(filename)
-        if not os.path.exists(os.path.dirname(filename)):
-            try:
-                os.makedirs(os.path.dirname(filename))
-            except OSError as exc: # Guard against race condition
-                if exc.errno != errno.EEXIST:
-                    raise
-
-        with open(filename, 'wb') as f:
-            if 'http' not in url:
-                # sometimes an image source can be relative 
-                # if it is provide the base url which also happens 
-                # to be the site variable atm. 
-                url = '{}{}'.format(site, url)
-            response = requests.get(url)
-            f.write(response.content)
+            with open(filename, 'wb') as f:
+                if 'http' not in url:
+                    # sometimes an image source can be relative 
+                    # if it is provide the base url which also happens 
+                    # to be the site variable atm. 
+                    url = '{}{}'.format(site, url)
+                response = requests.get(url)
+                f.write(response.content)
     i=i+1
